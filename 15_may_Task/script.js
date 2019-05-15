8c9d792dd7a41461f44fb991d5c3f8b40d70ca26
unknown <rashadVg@code.edu.az>
2019-05-15 22:28:16 +0400

    task_15May

---
+++ 15_may_Task/script.js
@@ -0,0 +1,20 @@
+function myfunc(){
+    document.querySelector(".aside").style.marginLeft = "0px";
+    document.querySelector(".aside").style.transition = "1s";
+}
+function closeIcon(){
+    document.querySelector(".aside").style.marginLeft="-250px";
+    document.querySelector(".aside").style.transition = "1s";
+}
+function button2(){
+    document.querySelector(".aside").style.marginLeft="0px";
+    document.querySelector(".aside").style.transition = "1s";
+    document.querySelector("body").style.backgroundColor="rgb(104, 90, 90)";
+    document.querySelector("body").style.transition = "1s";
+}
+function closeIcon2(){
+    document.querySelector(".aside").style.marginLeft="-250px";
+    document.querySelector(".aside").style.transition = "1s";
+    document.querySelector("body").style.backgroundColor="white";
+    document.querySelector("body").style.transition="1s";
+}
